e1aa677ad871e1b09bc0e308503ff78be51c60bc
Glitch (glitch-in-bio) <none>
2022-12-06 12:20:42 +0000

    Exporting to glitchdotcom/glitch-in-bio ./vite.config.js:31317407/201 ./package.json:31317407/6

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,11 @@ export default defineConfig(async ({ command, mode }) => {
       exclude: ['./settings.json']
     },
     server: {
+      host:"0.0.0.0",
+      port:3000,
       strictPort: true,
       hmr: {
-        port: 443
+        clientPort: 443
       }
     }
   };
