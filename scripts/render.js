f33bca0a6e6f29fc6c3f111da46654adb39b59fd
vchanhk <vincentchan.eth@pm.me>
2024-07-04 02:57:54 -0600

    Update render.js

--- scripts/render.js
+++ scripts/render.js
@@ -1,5 +1,5 @@
 // Importing modules and functions
-import { html, render } from "lighterhtml";
+import { html, render } from "/lighterhtml.js";
 import { renderMeta } from "/templates/meta.js";
 import { renderHeader } from "/templates/header.js";
 import { renderLinks } from "/templates/links.js";
