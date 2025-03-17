a49195f2e291161233d736e465e0cb44701e89fc
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2025-03-16 18:47:13 -0600

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -37,7 +37,7 @@ let ytplayerdisabled = true;
 		videoplayer.setAttribute("rotation", "0 0 0");
 		videoplayer.setAttribute("position", "0 -3 0");
 		videoplayer.setAttribute("hand-controls", "true");
-		videoplayer.setAttribute("button-position", "0 3.05 -1.23");
+		videoplayer.setAttribute("button-position", "0 3.05 -1.2");
 		videoplayer.setAttribute("volume", "3");
 		videoplayer.setAttribute("button-rotation", "0 0 0");
 		videoplayer.setAttribute("button-scale", "2 2 2");
